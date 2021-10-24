bae8db4b562117855614165ea545988bd0a2b9ea
dendudko <dudko.do@students.dvfu.ru>
2021-10-24 23:29:03 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+function setFocus()
+{
+    document.getElementById("in").focus();
+}
+
+function lastMessageScroll(b) {
+    let e = document.querySelector('.wrapper_ScrollBottom');
+    if (!e) return;
+
+    e.scrollIntoView({
+        behavior: b || 'auto',
+        block: 'end',
+    });
+}
+
+function sendMessage(){
+    let date = new Date()
+    let pole = document.getElementById('in')
+    let text = document.createTextNode(pole.value)
+    let mb = document.getElementById('messagebox')
+    if (text.data.length>0) {
+        text.data = text.data + "&nbsp" + date.toLocaleTimeString().slice(0, 5)
+        mb.innerHTML += text.data + "<br><br>"
+        count++
+        localStorage.setItem('count', count)
+        localStorage.setItem(count, text.data)
+    }
+    pole.value = ""
+    lastMessageScroll()
+}
+
+addEventListener('keydown', function(key) {
+    if (key.keyCode === 13)
+        sendMessage()
+})
+
+if (localStorage.length>0) {
+    count = localStorage.getItem('count')
+}
+else {
+    count = 0
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    restoreFromLocalStorage();
+})
+
+function restoreFromLocalStorage(){
+    if (localStorage.length>0) {
+        for (let i = 1; i <= localStorage.getItem('count'); i++) {
+            document.getElementById('messagebox').innerHTML += localStorage.getItem(i.toString()) + "<br><br>"
+        }
+        lastMessageScroll()
+    }
+}
+
+function clearHistory(){
+    document.getElementById('messagebox').innerHTML=""
+    localStorage.clear()
+    fromNull()
+}
+
+function deleteLastMessage(){
+    fromNull()
+    localStorage.removeItem(count)
+    if (count!==0){count--}
+    localStorage.setItem('count', count)
+    if (count===0){
+        localStorage.removeItem('count')
+    }
+    restoreFromLocalStorage()
+}
+
+function fromNull()
+{
+    document.getElementById('messagebox').innerHTML = "Сделал почти с нуля, копируя исключительно то, что дописал сам...<br><br>"
+}
+
+
+
